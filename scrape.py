16423dc73469f9177ed18b251884b0f7b145da9a
Simon Skinner <codewith@vultuk.me>
2014-04-21 13:47:49 +0100

    Added comments, we now also download vine videos

--- scrape.py
+++ scrape.py
@@ -3,52 +3,71 @@ import urllib, urllib.request, json, sys, cgi, re
 import services.service as service
 import threading
 
+# Set up the variables we may need
+## Many of these are out-dated due to the new multi-threading system
 searchTerm = None
 dataID = '0'
 scroller = ''
 currentPage = 1
 
-searchTypes = ['TwitPic', 'Instagram']
+# Initiate the services we will search for
+searchTypes = ['TwitPic', 'Instagram', 'Vine']
 
-searchTerm = ""
-
-
-#if sys.argv[1:]:
-#        searchTerm = sys.argv[1]
-
-#searchTerm = urllib.parse.quote(searchTerm)
-
+# Class to handle scraping
 class TweetScrape:
     content = ""
     searchTerm = ""
     dataID = '0'
     scroller = ''
+
+    # Init the class and required variables
     def __init__(self, searchTerm):
+        # Store the search term requested
         self.searchTerm = searchTerm
+
+        # Run the scraper
         self.run()
 
+    # Function to run the scraper
     def run(self):
+        # Load the required URL based on the search term
         self.content = self.openUrl(self.searchTerm)
+
+        # Get a list of all URLS in the tweet listing and scrape them
         self.getImageShorts(self.content)
+
+        # Move on the the next page of tweets
         self.nextData(self.content)
 
+    # Function to move to the next page of tweets
     def nextData(self, content):
         global dataID, scroller
         oldID = self.dataID
         tempHold = []
+
+        # Get a list of all tweet IDs that we have checked
         m = re.findall(r'data-tweet-id=\\"([0-9]+)\\"', content.decode("utf-8"), re.M|re.I)
         for i in m:
             tempHold.append(int(i))
+
+        # Check we can get the required details to generate the next page
         try:
             dataID = tempHold[len(tempHold)-1]
+
+        # If we get an index error then we reset to page one
         except IndexError:
             dataID = 0
             currentPage = 1
             self.scroller = 0
+
+        # Create the details to request the next page of tweets
         else:
             self.scroller = "TWEET-%s-%s" % (dataID, tempHold[0])
+
+        # Run the scraper
         self.run()
 
+    # Check the page for the services we have requested
     def getImageShorts(self, content):
         global searchTypes, searchTrd
         for t in searchTypes:
@@ -60,6 +79,7 @@ class TweetScrape:
 
             #tr.hunt(self.content)
 
+    # Get the HTML from the page of tweets
     def openUrl(self, term, page=0):
         global dataID, scroller
         baseUrl = "https://twitter.com/i/search/timeline?f=realtime&src=typd&include_available_features=1&include_entities=1&scroll_cursor=%s&q=%s" % (self.scroller,term)
@@ -72,25 +92,27 @@ class TweetScrape:
             f.close()
         return buf
 
+# Function that does the multi-threading for search terms
 def keepRunning():
+    # Run infinitely
     while 1:
+        # Get a search term from the user
         input_term = input("Enter Search Term: ")
+
+        # Make sure the term is safe for web
         searchTerm = urllib.parse.quote(input_term)
 
+        # Let the user know we are working for them
         print ("Searching for images using term %s." % (input_term))
         print ()
 
+        # Send the serch term to the Scraper in a new thread
         downThread = threading.Thread(target=TweetScrape, args=(searchTerm,))
         downThread.deamon = True
         downThread.start()
 
-        #tt = TweetScrape(searchTerm)
 
-
+# Create the multi-threading environment to keep the service running
 keepRun = threading.Thread(target=keepRunning)
 keepRun.deamon = True
 keepRun.start()
-
-
-
-    #tt = TweetScrape(searchTerm)
--- services/service.py
+++ services/service.py
@@ -2,67 +2,117 @@ import urllib, urllib.request, re, os
 
 class Service:
 
+    # Initialise the service
     def __init__(self, folder):
+        # Set the folder name
         self.folder = folder
+
+        #Create an array to hold picture urls
         self.picUrls = []
+
+        # Make the directories if we can
         try:
             os.makedirs(self.folder)
         except OSError:
             pass
 
+    # Function to download content from an url
     def openUrl(self, url):
+        # Create a buffer variable
         buf = ""
+
+        # Try to optn the url
         try:
             f       = urllib.request.urlopen(url)
+
+        # If we get a 404 error we just skip the download
         except urllib.request.URLError:
             pass
+
+        # Read the html content into the buffer
         else:
             buf     = f.read()
             f.close()
+
+        # return the buffer
         return buf
 
+    # Set the url into a local variable
     def setUrl(self, url):
         self.url = url
 
+    # Saves the img / vid to a local file
     def savePicture(self, url, fullUrl):
         if url:
+            # Find a matching url
             m = re.match(self.htmlPic, url, re.M|re.I)
+
+            # If we have a match
             if m:
+                # Set the filename
                 filename = m.group(1)
+
+                # Check if we already have the file
                 if os.path.exists("%s/%s" % (self.folder,filename)) == False:
-                    #print ("\033[32mImage from %s saved as %s.\033[0m" % (fullUrl,filename))
+
+                    # Open a file to write into
                     f = open("%s/%s" % (self.folder,filename), 'wb')
+
+                    # See if we can read the file
                     try:
                         picLink = urllib.request.urlopen(url).read()
+
+                    # If we get a 404 then ignore
                     except urllib.request.URLError:
                         pass
+
+                    # Write the contents of the img / vid to a local file
                     else:
                         f.write(picLink)
+
+                    # Close the local file
                     f.close()
     
+    # Creates the correct url to hunt down
     def createPicUrl(self, shortUrl):
         if self.postRegUrl:
             return self.postRegUrl % shortUrl
         else:
             return shorturl
 
+    # Hunt for the link to the img / vid file
     def picFromHtml(self, url):
+        # Open the content of the url
         content = self.openUrl(url)
+
+        # Make sure we have real content that can be decoded
         if hasattr(content, 'decode'):
+
+            # Search the content for the given htmlUrl and return the file name
             m = re.search(self.htmlUrl, content.decode("utf-8"), re.M|re.I)
             if m:
                 return m.group()
         else:
             return ""
 
+    # Function called when the content of the page has been downloaded
     def hunt(self, content):
+        # Search the content for the given service Regexp linking to the service page
         m = re.findall(self.regexp, content.decode("utf-8"), re.M|re.I)
+
+        # If we find a service link
         if m:
             for i in m:
+                # Hunt for the link to the img / vid file
                 d = self.picFromHtml(self.createPicUrl(i))
+
+                # Save the img / vid to the local file system
                 self.savePicture(d,self.createPicUrl(i))
+
+        # Returns the amount of img / vids found
         return len(self.picUrls)
 
+# Create a service for TwitPic
 class TwitPic(Service):
     regexp     = r'pic\.twitter\.com\\\/([a-zA-Z0-9]+)\\u003c'
     htmlUrl    = r'https://pbs\.twimg\.com/media/([a-zA-Z0-9_-]+)\.jpg'
@@ -70,9 +120,18 @@ class TwitPic(Service):
     postRegUrl = 'http://pic.twitter.com/%s'
     pass
 
+# Create a service for Instagram
 class Instagram(Service):
     regexp     = r'url=\\"http:\\\/\\\/instagram\.com\\\/p\\\/([a-zA-Z0-9]+)\\\/'
     htmlUrl    = r'http://distilleryimage[0-9]+\.ak\.instagram\.com/([a-zA-Z0-9_-]+)\.jpg'
     htmlPic    = r'http://distilleryimage[0-9]+\.ak\.instagram\.com/(.*)'
     postRegUrl = 'http://instagram.com/p/%s'
+    pass
+
+# Create a service for Vine
+class Vine(Service):
+    regexp     = r'vine\.co\\\/v\\/([a-zA-Z0-9]+)\\u003c'
+    htmlUrl    = r'https://v\.cdn\.vine\.co/r/videos/([a-zA-Z0-9_.-]+)\.mp4'
+    htmlPic    = r'https://v\.cdn\.vine\.co/r/videos/(.*)'
+    postRegUrl = 'http://vine.co/v/%s'
     pass
